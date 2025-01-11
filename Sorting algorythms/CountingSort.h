adb9da3fb3ec73aa62af965bd2dd9019012583bd
Tymek <tymeksondel@gmail.com>
2025-01-11 15:40:10 +0100

    counting sort succesfully tested

--- Sorting algorythms/CountingSort.h
+++ Sorting algorythms/CountingSort.h
@@ -1,1 +1,46 @@
 #pragma once
+
+int findMax(int* array, int size) {
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int findMin(int* array, int size) {
+    int min = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int* countingSort(int* array, int size) {
+    int min = findMin(array, size);
+    int max = findMax(array, size);
+
+    int* arrayCount = new int[max - min + 1](); 
+
+    for (int i = 0; i < size; i++) {
+        arrayCount[array[i] - min]++;
+    }
+
+    int* arrayCopy = new int[size];
+    int index = 0;
+
+    for (int i = 0; i <= max - min; i++) {
+        while (arrayCount[i] > 0) {
+            arrayCopy[index++] = i + min;
+            arrayCount[i]--;
+        }
+    }
+
+    delete[] arrayCount;
+
+    return arrayCopy;
+}
--- Sorting algorythms/main.cpp
+++ Sorting algorythms/main.cpp
@@ -1,20 +1,40 @@
 #include<iostream>
 #include"HeapSort.h"
+#include"CountingSort.h"
 #include"BinaryHeap/BinaryHeap.h"
 #include"BinaryHeap/DynamicArray.h"
 
 using namespace std;
 
 int main() {
+	// heapsort
 	Heap<int> testHeap;
 
 	for (int i = 0; i < 20; i++) {
-		testHeap.insert(rand()%20);
+		testHeap.insert(rand()%100);
 	}
 
 	testHeap.prettyPrint();
 
 	DynamicArray<int> sorted = heapsort(testHeap);
 	cout << sorted.str(20) << endl;
+
+	//countin sort
+	int* testArray = new int[100]();
+
+	for (int i = 0; i < 100; i++) {
+		testArray[i] = rand() % 100;
+	}
+
+	int* sortedArray = countingSort(testArray, 100);
+
+	cout << "Before:\t\tAfter:" << endl;
+	for (int i = 0; i < 100; i++) {
+		cout << testArray[i] << " \t\t";
+		cout << sortedArray[i] << "  " << endl;
+	}
+
+	delete[] sortedArray;
+
 	return 0;
 }
